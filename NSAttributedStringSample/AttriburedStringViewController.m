b14f3242a6c29822fb649c4d900b72c249796f55
UjwalManjunath <muddu1990@gmail.com>
2013-02-07 06:48:25 -0500

    intial Commit

--- NSAttributedStringSample/AttriburedStringViewController.m
+++ NSAttributedStringSample/AttriburedStringViewController.m
@@ -9,21 +9,85 @@
 #import "AttriburedStringViewController.h"
 
 @interface AttriburedStringViewController ()
+@property (weak, nonatomic) IBOutlet UIStepper *wordStepper;
+@property (weak, nonatomic) IBOutlet UILabel *selectedWordlabel;
+@property (weak, nonatomic) IBOutlet UILabel *contentLabel;
 
 @end
 
 @implementation AttriburedStringViewController
 
-- (void)viewDidLoad
+-(void)addLabelAttribures:(NSDictionary *)attributes range:(NSRange)range
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    if(range.location != NSNotFound)
+    {
+        NSMutableAttributedString *mutableAttributedString = [self.contentLabel.attributedText mutableCopy];
+        [mutableAttributedString addAttributes:attributes range:range];
+        self.contentLabel.attributedText = mutableAttributedString;
+    }
+
+}
+-(void) addSelectedLabelAttribute:(NSDictionary *)attributes
+{
+    
+    NSRange range = [[self.contentLabel.attributedText string]rangeOfString:[self selectedWord]];
+     [self addLabelAttribures:attributes range:range];
+}
+- (IBAction)updateColor:(UIButton *)sender
+{
+    [self addSelectedLabelAttribute:@{NSForegroundColorAttributeName:[sender backgroundColor]}];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)underline
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self addSelectedLabelAttribute:@{NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle)}];
+    
 }
+- (IBAction)updateFont:(UIButton *)sender
+{
+    CGFloat fontSize = [UIFont systemFontSize];
+    NSDictionary *attribute = [self.contentLabel.attributedText attributesAtIndex:0 effectiveRange:NULL];
+    UIFont *existingFont = attribute[NSFontAttributeName] ;
+        if(existingFont) fontSize = existingFont.pointSize;
+    UIFont *font = [sender.titleLabel.font fontWithSize:fontSize ];
+                            
+                            
+                            [self addSelectedLabelAttribute:@{ NSFontAttributeName  :font}];
+    
+    
+}
+
+- (IBAction)noUnderline
+{
+    [self addSelectedLabelAttribute:@{NSUnderlineStyleAttributeName: @(NSUnderlineStyleNone)}];
+}
+
+
+-(NSArray *)wordList
+{
+    NSArray *wordList = [[self.contentLabel.attributedText string]componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if([wordList count])
+        return wordList;
+    else
+        return @[@""];
+}
+- (IBAction)updateSelectedWord
+{
+     self.wordStepper.maximumValue = [[self wordList]count]-1;
+    self.selectedWordlabel.text = [self selectedWord];
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self updateSelectedWord];
+}
+
+
+-(NSString *)selectedWord
+{
+    return [[self wordList] objectAtIndexedSubscript:(int)self.wordStepper.value];
+}
+
 
 @end
